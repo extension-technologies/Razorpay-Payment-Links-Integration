01bc99c845c2cb70d071c5db52c2266533f51cc2
sameer Chauhan <sameer55chauhan@gmail.com>
2023-06-02 12:44:52 +0530

    handling get

--- razorpay_payment_links_integration/razorpay_payment_links_integration/doctype/razorpay_payment_links/razorpay_payment_links.py
+++ razorpay_payment_links_integration/razorpay_payment_links_integration/doctype/razorpay_payment_links/razorpay_payment_links.py
@@ -12,7 +12,7 @@ class RazorpayPaymentLinks(Document):
 					pe_doc = frappe.get_doc('Payment Entry', self.get('payment_for'))
 					if(pe_doc.razorpay_payment_status != "Paid" and pe_doc.docstatus != 1):
 						pe_doc.razorpay_payment_status = "Paid"
-						pe_doc.reference_no = self.reference_no if self.reference_no else ""
+						pe_doc.reference_no = self.get('reference_no') if self.get('reference_no') else ""
 						pe_doc.reference_date = self.get('reference_date') if self.get('reference_date') else None
 						pe_doc.docstatus = 1
 						pe_doc.save(ignore_permissions=True)
